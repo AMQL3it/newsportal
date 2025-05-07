5a095305a12dc12c44e6f661e290b4f41cb19106
AMQL3it <atiqurrahmanrasel19@gmail.com>
2025-05-07 15:37:03 +0600

    middleware - generic dataFilterHandler methods

---
+++ server/src/common/dataFilterHandler.js
@@ -0,0 +1,20 @@
+const { validationResult } = require("express-validator");
+
+const dataFilterHandler = (req, res, next) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    const formattedErrors = errors.array().map((error) => ({
+      field: error.param,
+      message: error.msg,
+    }));
+
+    return res.status(400).json({
+      status: "error",
+      message: "Validation failed.",
+      errors: formattedErrors,
+    });
+  }
+  next();
+};
+
+module.exports = dataFilterHandler;
