06bd3f917de308762ce70231667862fd856b252a
AMQL3it <atiqurrahmanrasel19@gmail.com>
2025-05-10 09:59:46 +0600

    FE - Vite set up

---
+++ web/src/App.jsx
@@ -0,0 +1,38 @@
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import Category from "./components/Dashboard/Category";
+import PostManagement from "./components/Dashboard/PostManagement";
+import Tags from "./components/Dashboard/Tag";
+import User from "./components/Dashboard/User";
+import Dashboard from "./pages/Dashboard";
+// import ExplorePage from "./pages/ExplorePage";
+// import HomePage from "./pages/HomePage";
+// import Layout from "./pages/Layout";
+import Login from "./pages/Login";
+import Varification from "./pages/Varification";
+// import CategoryPage from './pages/CategoryPage';
+// import NewsDetails from './pages/NewsDetails';
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        {/* <Route path="/" element={<Layout />}>
+          <Route exect path="" element={<HomePage />} />
+          <Route exect path="newsfeed" element={<ExplorePage />} />
+        </Route> */}
+        <Route path="/login" element={<Login />} />
+        <Route path="/varification" element={<Varification />} />
+        <Route path="/dashboard" element={<Dashboard />}>
+          <Route path="categories" element={<Category />} />
+          <Route path="tags" element={<Tags />} />
+          <Route path="posts" element={<PostManagement />} />
+          <Route path="users" element={<User />} />
+
+          <Route path="*" element={<div>404</div>} />
+        </Route>
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
