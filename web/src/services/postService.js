d85b399de62887a8a94996cc20519d0e291dc2b0
AMQL3it <atiqurrahmanrasel19@gmail.com>
2025-05-14 12:28:58 +0600

    FE - add getAllByCategory methods

--- web/src/services/postService.js
+++ web/src/services/postService.js
@@ -2,6 +2,7 @@ import apiService from "./apiService"; // Generic axios wrapper
 
 const postService = {
   getAll: () => apiService.getAll("posts"),
+  getAllByCategory: (id) => apiService.getAll(`posts/category/${id}`),
   getById: (id) => apiService.getById("posts", id),
   create: (data, config) => apiService.create("posts", data, config),
   update: (id, data) => apiService.update("posts", id, data),
